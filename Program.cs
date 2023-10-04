c7b65077f65711b8167ef84b6f14da93f03575fb
Dominic Reinmann <97438001+DominicReinmann@users.noreply.github.com>
2023-10-04 22:36:58 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,42 +5,48 @@ var filePath = (string)null;
 
 if (fileName == null)
 {
-    Console.WriteLine("Filepath: ");
-    filePath = Console.ReadLine().Replace("\"","");
+  Console.WriteLine("Filepath: ");
+  filePath = Console.ReadLine().Replace("\"","");
 }
 else
 {
-    filePath = Directory.GetCurrentDirectory() + "\\" + fileName;
+  filePath = Directory.GetCurrentDirectory() + "\\" + fileName;
 }
 
 // 
-string[] keyWords = { };
+string[] keyWords =
+{
+  "creat", "alter", "drop", "truncate", "insert", "delete", "update", "savepoint", "commit", "rollback", "grant", "revoke"
+  , "select", "count", "sum", "avg", "max", "min", "group by", "having", "order by", "where", "and", "or", "between", "like", "in"
+  , "is null", "is not null", "inner join", "left join", "right join", "full join", "union", "from", "as", "cast", "right", "left"
+  , "on", "end", "when", "case", "if", "else", "begin", "then", "declare", "null", "close", "asc", "fetch", "next", "into", "open"
+  , "for", "cursor", "table", "over"
+};
 
 void ReplaceInFile(string filePath, string searchText, string replaceText)
 {
-    StreamReader reader = new StreamReader(filePath);
-    string content = reader.ReadToEnd();
-    reader.Close();
+  StreamReader reader = new StreamReader(filePath);
+  string content = reader.ReadToEnd();
+  reader.Close();
 
-    content = Regex.Replace(content, $@"(?<!')\b{searchText}\b(?!')", replaceText); // regex for sql reco
+  content = Regex.Replace(content, $@"(?<!')\b{searchText}\b(?!')", replaceText); // regex for sql reco
 
-    StreamWriter writer = new StreamWriter(filePath);
-    writer.Write(content);
-    writer.Close();
+  StreamWriter writer = new StreamWriter(filePath);
+  writer.Write(content);
+  writer.Close();
 }
 
 foreach (string keyWord in keyWords)
 {
-    var wordLower = keyWord.ToLower();
-    var wordUpper = keyWord.ToUpper(); 
-    ReplaceInFile(filePath, wordLower, wordUpper);
+  var wordLower = keyWord.ToLower();
+  var wordUpper = keyWord.ToUpper(); 
+  ReplaceInFile(filePath, wordLower, wordUpper);
 }
 foreach (string keyWord in keyWords)
 {
-    string wordLower = keyWord.Substring(0,1);
-    string rest = keyWord.Substring(1);
-    wordLower = wordLower.ToUpper();
-    string wordUpper = keyWord.ToUpper();
-    ReplaceInFile(filePath, wordLower, wordUpper);
+  string wordLower = keyWord.Substring(0,1);
+  string rest = keyWord.Substring(1);
+  wordLower = wordLower.ToUpper();
+  string wordUpper = keyWord.ToUpper();
+  ReplaceInFile(filePath, wordLower, wordUpper);
 }
-
