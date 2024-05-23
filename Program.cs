cb1abcb973d6e912833357b2ea972e119ecc868c
Dominic Reinmann <reinmann.dominic@bluewin.ch>
2024-05-23 22:29:53 +0200

    cleanup and added LICENSE

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection;
-using System.Text.Json;
+﻿using System.Text.Json;
 using System.Text.RegularExpressions;
 using CommandLine;
 using SQLConverter;
@@ -8,6 +7,7 @@ public class Program
 {
     public static async Task<int> Main(string[] args)
     {
+        CheckForFile();
         if (args.Length == 1 && !args[0].StartsWith("-"))
         {
             args = new[] { "-p", args[0] };
@@ -58,7 +58,6 @@ public class Program
                     return 0;
                 }
             }
-
             if (File.Exists(options.FilePath))
             {
                 ConvertSqlFile(options.FilePath);
@@ -81,10 +80,12 @@ public class Program
     {
         var input = Console.ReadLine();
         string[] inputArgs = input.Split(' ');
+
         if (inputArgs.Length == 1 && !inputArgs[0].StartsWith("-"))
         {
             inputArgs = new[] { "-p", inputArgs[0].Replace("\"", "") };
         }
+
         options = Parser.Default.ParseArguments<ConverterOptions>(inputArgs).MapResult(
             parsedOptions => parsedOptions,
             _ => options);
@@ -100,7 +101,25 @@ public class Program
         return 1;
     }
 
-    static void ConvertSqlFile(string filePath)
+    private static void CheckForFile()
+    {
+        if (!Directory.Exists("C:\\var\\SqlConverter"))
+        {
+            Directory.CreateDirectory("C:\\var\\SqlConverter");
+        }
+
+        if (!File.Exists("C:\\var\\SqlConverter\\keywords.json"))
+        {
+            using (StreamWriter sw = File.CreateText("C:\\var\\SqlConverter\\keywords.json"))
+            {
+                var sqlKeywords = GetKeywords();
+                string json = JsonSerializer.Serialize(sqlKeywords);
+                sw.Write(json);
+            }
+        }
+    }
+
+    public static void ConvertSqlFile(string filePath)
     {
         try
         {
@@ -131,12 +150,44 @@ public class Program
                 ReplaceInFile(filePath, wordLower, wordUpper);
             }
 
-            Console.WriteLine("Successfully Converted");
-            Console.WriteLine("version 1.1");
+            Console.WriteLine("Successfully Converted File");
         }
         catch (Exception ex)
         {
             Console.WriteLine("Error converting file: " + ex.Message);
         }
+    }
+
+    private static string[] GetKeywords()
+    {
+        return new string[]
+        {
+            "ADD", "ALL", "ALTER", "AND", "ANY", "AS", "ASC", "AUTHORIZATION",
+            "BACKUP", "BEGIN", "BETWEEN", "BREAK", "BROWSE", "BULK", "BY", "CASCADE",
+            "CASE", "CHECK", "CHECKPOINT", "CLOSE", "CLUSTERED", "COALESCE", "COLLATE",
+            "COLUMN", "COMMIT", "COMPUTE", "CONSTRAINT", "CONTAINS", "CONTAINSTABLE",
+            "CONTINUE", "CONVERT", "CREATE", "CROSS", "CURRENT", "CURRENT_DATE",
+            "CURRENT_TIME", "CURRENT_TIMESTAMP", "CURRENT_USER", "CURSOR", "DATABASE",
+            "DBCC", "DEALLOCATE", "DECLARE", "DEFAULT", "DELETE", "DENY", "DESC",
+            "DISK", "DISTINCT", "DISTRIBUTED", "DOUBLE", "DROP", "DUMP", "ELSE",
+            "END", "ERRLVL", "ESCAPE", "EXCEPT", "EXEC", "EXECUTE", "EXISTS",
+            "EXIT", "EXTERNAL", "FETCH", "FILE", "FILLFACTOR", "FOR", "FOREIGN",
+            "FREETEXT", "FREETEXTTABLE", "FROM", "FULL", "FUNCTION", "GOTO", "GRANT",
+            "GROUP", "HAVING", "HOLDLOCK", "IDENTITY", "IDENTITY_INSERT", "IDENTITYCOL",
+            "IF", "IN", "INDEX", "INNER", "INSERT", "INTERSECT", "INTO", "IS",
+            "JOIN", "KEY", "KILL", "LEFT", "LIKE", "LINENO", "LOAD", "MERGE", "NATIONAL",
+            "NOCHECK", "NONCLUSTERED", "NOT", "NULL", "NULLIF", "OF", "OFF", "OFFSETS",
+            "ON", "OPEN", "OPENDATASOURCE", "OPENQUERY", "OPENROWSET", "OPENXML",
+            "OPTION", "OR", "ORDER", "OUTER", "OVER", "PERCENT", "PIVOT", "PLAN",
+            "PRECISION", "PRIMARY", "PRINT", "PROC", "PROCEDURE", "PUBLIC", "RAISERROR",
+            "READ", "READTEXT", "RECONFIGURE", "REFERENCES", "REPLICATION", "RESTORE",
+            "RESTRICT", "RETURN", "REVERT", "REVOKE", "RIGHT", "ROLLBACK", "ROWCOUNT",
+            "ROWGUIDCOL", "RULE", "SAVE", "SCHEMA", "SECURITYAUDIT", "SELECT", "SESSION_USER",
+            "SET", "SETUSER", "SHUTDOWN", "SOME", "STATISTICS", "SYSTEM_USER", "TABLE",
+            "TABLESAMPLE", "TEXTSIZE", "THEN", "TO", "TOP", "TRAN", "TRANSACTION",
+            "TRIGGER", "TRUNCATE", "TRY_CONVERT", "TSEQUAL", "UNION", "UNIQUE", "UNPIVOT",
+            "UPDATE", "UPDATETEXT", "USE", "USER", "VALUES", "VARYING", "VIEW",
+            "WAITFOR", "WHEN", "WHERE", "WHILE", "WITH", "WRITETEXT"
+        };
     }
 }
--- obj/Debug/net6.0/SQLConverter.AssemblyInfo.cs
+++ obj/Debug/net6.0/SQLConverter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dominic Reinmann")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1+964175d2a03ab426b549b8aca48fc367965323dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1+0feb524e0b763b763943602ea58183e7f25e8ea1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SQLConverter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SQLConverter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.1.0")]
